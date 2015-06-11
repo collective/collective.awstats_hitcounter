54f4e4ee9d232073618f0fad6d5fd6d8c9cf4596
pigeonflight <david@alteroo.com>
2015-06-11 21:28:12 +0000

    added ability to count download attachments

--- src/collective/awstats_hitcounter/browser/static/app.js
+++ src/collective/awstats_hitcounter/browser/static/app.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
+  function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+  }
   $("#awstats_hitcounter").hide();
   var pathname = window.location.pathname;
 
   if (window.location.pathname == '/'){ var pathname = "";}
-  $.getJSON( "awstats_hitcounter_view", function( data ) {
+  if (endsWith(pathname,'/view')){
+       pathname = pathname.substring(0, pathname.length - 5);
+   }  
+    //console.log(pathname+"/awstats_hitcounter_view");
+  $.getJSON( pathname+"/awstats_hitcounter_view", function( data ) {
 
    if (data.success == 'true'){
  
@@ -11,14 +18,24 @@ $(document).ready(function(){
       "class": "creation-date",
       html: data.creation_date
     }).appendTo( "#awstats_hitcounter .date-created" );
+       
     $( "<span/>", {
       "class": "modification-date",
       html: data.modification_date
     }).appendTo( "#awstats_hitcounter .date-modified" );
+       
     $( "<span/>", {
       "class": "views-count-for-pages",
       html: data.page_views
     }).appendTo( "#awstats_hitcounter .page-views" );
+    if ( data.hasOwnProperty('downloads') ) {
+        $( "<span/>", {
+          "class": "views-count-for-downloads",
+          html: data.downloads
+        }).appendTo( "#awstats_hitcounter .attachment-downloads" );
+        $(".page-info.awstats-downloads").show();
+
+    }
     $("#awstats_hitcounter").show();
 
    }
